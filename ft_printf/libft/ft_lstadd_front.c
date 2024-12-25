acce3c6dc577dc8c2496a3e19629c2f4c09aaa3d
eutrius <eutrius@gmail.com>
2024-12-26 00:46:00 +0100

    set up project structur ft_printf

---
+++ ft_printf/libft/ft_lstadd_front.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyriarte <jyriarte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 20:15:16 by jyriarte          #+#    #+#             */
+/*   Updated: 2024/12/25 16:12:45 by jyriarte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new_node)
+{
+	if (!new_node)
+		return ;
+	new_node->next = *lst;
+	*lst = new_node;
+}
---
+++ ft_printf/src/ft_printf.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyriarte <jyriarte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/25 16:21:42 by jyriarte          #+#    #+#             */
+/*   Updated: 2024/12/25 23:52:15 by jyriarte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "libftprintf.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+int	print_format(int c, va_list args);
+int	ft_printf(const char *str, ...)
+{
+	va_list	args;
+	int		i;
+	int		count;
+
+	count = 0;
+	i = 0;
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			i++;
+			count += print_format(str[i], args);
+		}
+		else
+		{
+			ft_putchar_fd(str[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
